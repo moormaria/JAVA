89f5ba4aab7a143f3abdc1b3710a6f81a2928aa9
moormaria <mashunyamoor@gmail.com>
2024-12-08 12:00:44 +0300

    javadoc

---
+++ L1 - 4 - Документирование кода/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Greeting","l":"Greeting()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Greeting","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
